8162011c866cb9a5401607154840d9d9bc47057e
GabeRundlett <gabe@rundlett.com>
2022-09-21 12:03:16 -0600

    Cleanup

--- shaders/utils/voxel_edit.glsl
+++ shaders/utils/voxel_edit.glsl
@@ -26,7 +26,7 @@ f32 sd_gear(vec3 p, float radius1, float radius2, f32 notch_n, f32 notch_fac) {
     float fa = (atan(q.y, q.x) + an * 0.5) / an;
     float sym = an * floor(fa);
     vec2 r = mat2(cos(sym), -sin(sym), sin(sym), cos(sym)) * q;
-    f32 d = length(max(abs(r - vec2(radius1, 0)) - vec2(radius2 * 0.5, y * n * 4.0 * notch_fac), 0.0));
+    f32 d = radius1 * length(max(abs(r - vec2(radius1, 0)) - vec2(radius2 * 0.5, y * n * 4.0 * notch_fac), 0.0));
     d = d - 0.001;
     d = max(d, -sd_plane(p + f32vec3(0, 0, 1)));
     d = max(d, +sd_plane(p - f32vec3(0, 0, 1)));
@@ -43,7 +43,16 @@ f32 sd_castle_wall(in f32vec3 p) {
     f32vec3 t_p = p;
     t_p.x = t_p.x = (fract(t_p.x / t_repeat_x + 0.5) - 0.5) * t_repeat_x;
 
-    val = min(val, sd_box(t_p, f32vec3(t_repeat_x, 1, 4)));
+    val = min(val, sd_box(t_p - f32vec3(0.0, 0.0, 0.0), f32vec3(t_repeat_x, 1, 4)));
+    val = min(val, sd_box(t_p - f32vec3(0.0, +1.0, 4.0), f32vec3(t_repeat_x, 0.1, 1)));
+    val = min(val, sd_box(t_p - f32vec3(0.0, -1.0, 4.0), f32vec3(t_repeat_x, 0.1, 1)));
+
+    // wub_* = wall upper battlements
+    f32 wub_repeat_x = 1.2;
+    f32vec3 wub_p = p;
+    wub_p.x = wub_p.x = (fract(wub_p.x / wub_repeat_x + 0.5) - 0.5) * wub_repeat_x;
+    val = min(val, sd_box(wub_p - f32vec3(0.0, +1.0, 5.0), f32vec3(0.45, 0.1, 0.4)));
+    val = min(val, sd_box(wub_p - f32vec3(0.0, -1.0, 5.0), f32vec3(0.45, 0.1, 0.4)));
 
     // wlb_* = wall lower battlements
     f32 wlb_repeat_x = 4;
@@ -53,8 +62,6 @@ f32 sd_castle_wall(in f32vec3 p) {
     val = min(val, sd_triangular_prism(wlb_p, 1.85, 1));
     val = min(val, sd_box(wlb_p + f32vec3(0, 0, 2.8), f32vec3(1, 1.6, 2)));
 
-    // wub_* = wall upper battlements
-
     val = min(val, sd_cylinder(t_p, 4, 8));
     val = max(val, -sd_cylinder(t_p, 3.2, 8));
 
@@ -62,28 +69,6 @@ f32 sd_castle_wall(in f32vec3 p) {
 
     val = max(val, -sd_plane(p - f32vec3(0, 0, -4)));
     return val;
-    // f32vec3 p = abs(voxel_p - GLOBALS.pick_pos) - PLAYER.edit_radius;
-    // f32 wall_x = voxel_p.y - GLOBALS.edit_origin.y;
-    // f32 val = 0;
-    // val += f32(p.z > 1.0);
-    // val += f32(wall_x > +2.0 || wall_x < -2.0);
-    // val += min(f32(fract(voxel_p.x) > 0.5) + f32(wall_x < +1.9 && wall_x > -1.9), f32(voxel_p.z > 23.75));
-    // val += min(f32(wall_x < +1.9 && wall_x > -1.9), f32(voxel_p.z > 22.5));
-    // val += f32(voxel_p.z > 24.0);
-    // val -= f32(max(p.x, p.x) < 1.0);
-    // val = -200 * f32(val < 0.0) + 0.5;
-    // f32vec2 c_uv = voxel_p.xy - f32vec2(round(GLOBALS.pick_pos.x / 24) * 24, GLOBALS.edit_origin.y);
-    // f32 c_r = length(c_uv);
-    // f32 angle = (atan(c_uv.y, c_uv.x) / 3.14159 * 0.5) + 0.5;
-    // val += f32(voxel_p.z > 30.0);
-    // val += f32((fract(angle * 20 - 0.25) < 0.5) && voxel_p.z > 29.75);
-    // val -= f32(c_r < 4 && (c_r > 3.8 || (voxel_p.z < 28.75 && voxel_p.z > 28.5)) && (wall_x > +0.5 || wall_x < -0.5 || voxel_p.z > 24.7));
-    // f32 stair_angle = angle * 45 - 0.25;
-    // f32 stair_height = floor(stair_angle) * 0.2 / 3;
-    // val -= f32(voxel_p.z < 28.75 && c_r < 4 && c_r > 2.5 && (fract(stair_angle) < 0.75) && fract(voxel_p.z / 4) * 4 > 0.75 + stair_height && fract(voxel_p.z / 4) * 4 < 1 + stair_height);
-    // val = -200 * f32(val < 0.0) + 0.5;
-    // val += f32(voxel_p.z < 4.0) * 10000;
-    // return val;
 }
 
 b32 brush_should_edit(in f32vec3 voxel_p) {
@@ -91,10 +76,7 @@ b32 brush_should_edit(in f32vec3 voxel_p) {
     b32 result = false;
     f32vec3 pick_pos = floor(GLOBALS.pick_pos * VOXEL_SCL) / VOXEL_SCL;
     f32vec3 p = voxel_p - GLOBALS.edit_origin;
-    // result = sd_sphere(voxel_p - pick_pos, PLAYER.edit_radius) < 0.0;
-    // result = sd_cylinder(p, PLAYER.edit_radius, PLAYER.edit_radius) < 0.0;
-    // result = sd_sphere(voxel_p - pick_pos, PLAYER.edit_radius) + brush_noise_value(voxel_p) * 5.0 < 0.0;
-    result = sd_castle_wall(p) + (brush_noise_value(voxel_p) + 1.01) * 0.0 < 0.0;
+    result = sd_castle_wall(p) + abs(brush_noise_value(voxel_p)) * 0.0 < 0.0;
     return result && (sd_box(voxel_p - pick_pos, f32vec3(8, 8, 8)) < 0.0);
 }
 u32 brush_id_kernel(in f32vec3 voxel_p) {
