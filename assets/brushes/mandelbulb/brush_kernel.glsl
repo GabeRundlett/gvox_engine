b969641c3b54e1893a2aa718981bd85a116c6831
GabeRundlett <gabe@rundlett.com>
2022-10-29 01:42:06 -0600

    Add mandelbulb coloring

--- assets/brushes/mandelbulb/brush_kernel.glsl
+++ assets/brushes/mandelbulb/brush_kernel.glsl
@@ -14,11 +14,13 @@ b32 mandelbrot(in f32vec3 c) {
     return i == 100;
 }
 
-b32 mandelbulb(in f32vec3 c) {
+b32 mandelbulb(in f32vec3 c, in out f32vec3 color) {
     f32vec3 z = c;
     u32 i = 0;
     const f32 n = 8;
-    const u32 MAX_ITER = 20;
+    const u32 MAX_ITER = 4;
+    f32 m = dot(z, z);
+    f32vec4 trap = f32vec4(abs(z), m);
     for (; i < MAX_ITER; ++i) {
         f32 r = length(z);
         f32 p = atan(z.y / z.x);
@@ -28,9 +30,12 @@ b32 mandelbulb(in f32vec3 c) {
             sin(n * t) * sin(n * p),
             cos(n * t));
         z = z * pow(r, n) + c;
-        if (dot(z, z) > 4)
+        trap = min(trap, f32vec4(abs(z), m));
+        m = dot(z, z);
+        if (m > 256.0)
             break;
     }
+    color = f32vec3(m, trap.yz) * trap.w;
     return i == MAX_ITER;
 }
 
@@ -46,9 +51,15 @@ b32 custom_brush_should_edit(in BrushInput brush) {
     f32vec3 a = brush.p;
     a = a - OFFSET;
     f32vec3 c = a * 1.0 / BRUSH_SCL;
-    return mandelbulb(c);
+    f32vec3 color;
+    return mandelbulb(c, color);
 }
 
 Voxel custom_brush_kernel(in BrushInput brush) {
-    return Voxel(INPUT.settings.brush_color, BlockID_Stone);
+    f32vec3 a = brush.p;
+    a = a - OFFSET;
+    f32vec3 c = a * 1.0 / BRUSH_SCL;
+    f32vec3 color;
+    mandelbulb(c, color);
+    return Voxel(mix(1 - INPUT.settings.brush_color, INPUT.settings.brush_color, clamp(color.r, 0, 1)), BlockID_Stone);
 }
